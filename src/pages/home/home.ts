2f66b54e9ca1a5ec4f8e9b0e29cbaa7bf548c55f
Danilo <danilodegortes@gmail.com>
2021-08-27 17:58:24 +0200

    77

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Deploy } from 'cordova-plugin-ionic/dist/ngx';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,18 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public deploy: Deploy) {
 
   }
+
+  async performManualUpdate() {
+    const update = await this.deploy.checkForUpdate()
+    if (update.available){
+      console.log(update);
+      
+      console.log("yessssssssa!!!");
+      
+    }
+   }
 
 }
